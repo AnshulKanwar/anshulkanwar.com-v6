ca85fa30d3a53cd61e0ef2fe54f0999dc5fa6a2d
Anshul Kanwar <4n5hu1@protonmail.com>
2023-05-14 11:16:33 +0530

    fix typo 😭

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -43,7 +43,7 @@ const projects: Project[] = [
     url: "https://github.com/AnshulKanwar/lms",
     description:
       "A modern implementation of a Learning Management System (LMS)",
-    technologies: ["Djanog", "React"],
+    technologies: ["Django", "React"],
   },
 ];
 

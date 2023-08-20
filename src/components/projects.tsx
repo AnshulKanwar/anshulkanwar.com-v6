b903a5259cab61689f3042d9c02dd59f6e17b0dc
Anshul Kanwar <me@anshulkanwar.com>
2023-08-20 11:13:40 +0530

    fix paddings

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -51,7 +51,7 @@ const Projects = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-12">Projects</h1>
-      <div className="grid grid-cols-3 gap-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
         {projects.map(({ name, url, description, technologies }) => (
           <div key={name}>
             <a
